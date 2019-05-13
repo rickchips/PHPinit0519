a2a6d65cf46c6f247e3b340ce9fc6f66be9e2288
Richard <richard.lacarriere@gmail.com>
2019-05-13 15:09:08 +0200

    Variables - Constantes - Instructions de sortie - Conditions

--- index.php
+++ index.php
@@ -10,7 +10,57 @@
 <body>
 <h1>Ma page</h1>
 <?php
+
+
 echo  'Bonjour';
+
+$nb1 = 1.55;
+$isPublished = true;
+
+$content = "<p>Contenu du paragraphe : $nb1</p>";
+$content2 = '<p>Contenu du paragraphe : $nb1;</p>';
+
+echo $content;
+echo $content2;
+
+// Les constantes
+const NB_ARTICLES_PER_PAGES = 6;
+define("DATABASE_NAME", "php_project");
+
+echo NB_ARTICLES_PER_PAGES;
+echo DATABASE_NAME;
+
+// On ne modifie pas une constante
+// NB_ARTICLES_PER_PAGES = 12;
+
+// echo_FILE__;
+
+var_dump($content);
+print_r($content);
+
+$str1 = "Bonjour";
+$str2 = " tout le monde";
+
+echo "Bonjour : " . $str2;
+
+
+
+
+$temp=50;
+
+
+if($temp>=50) {
+    echo "il fait trop chaud";
+}  elseif ($temp>= 20){
+    echo "il fait chaud";
+}  elseif ($temp>=0){
+    echo "il fait idéal";
+}  else{
+    echo "il fait froid";
+}
+
+
+
 ?>
 
 </body>
